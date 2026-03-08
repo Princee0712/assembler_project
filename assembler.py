d1579fa5f96f8be5a647c4629edb68d8ea2ae5a7
Prince <prince25394@gmail.com>
2026-03-08 23:44:04 +0530

    Completed assembler interpretation

--- assembler.py
+++ assembler.py
@@ -20,13 +20,13 @@ program = readfile(inputfile)
 # convert num into binary
 def binary(num,bit):
     n = int(num)
-    if n < 0
+    if n < 0:
         n = (1<<bit)+n
     b = bin(n)[2:]
     return b.zfill(bit)
 
-# checking immediate range
- def checkimm(num, bit, line):
+# checking immediate range 
+def checkimm(num, bit, line):
      n = int(num)
      low = -(2**(bit-1))
      high = (2**(bit-1))-1
@@ -36,26 +36,26 @@ def binary(num,bit):
 
 # halt validation
 haltcount = 0
- for i , line in enumerate(program,1):
+for i , line in enumerate(program,1):
      clean = line.replace(" ", "")
      if clean == "beqzero,zero,0":
          haltcount +=1
      if clean =="beqzero,zero,0" and i != len(program):
          error(i)
-     if haltcount == 0:
-         error (len(program))
+if haltcount == 0:
+    error (len(program))
 
 
 # label detection
-labletable = {}
+labeltable = {}
 address = 0 
 for i, line in enumerate(program):
     if ":" in line:
         parts = line.split(":", 1)
-        label = part[0].strip()
+        label = parts[0].strip()
         inst = parts[1].strip()
         
-        labletable[label] = address
+        labeltable[label] = address
         program[i] = inst 
     address += 4
     
@@ -64,8 +64,8 @@ for i, line in enumerate(program):
 
 registers = {
    "zero" : "00000", "ra" : "00001", "sp" : "00010", "tp" : "00100",
-   "gp" : "00011", "t0" : "00101", "t1" : " 00110", "t2" : "00111",
-   "t3" : "11100" , "t4" : "11101" , "t5" : " 11110", "t6" : "11111",
+   "gp" : "00011", "t0" : "00101", "t1" : "00110", "t2" : "00111",
+   "t3" : "11100" , "t4" : "11101" , "t5" : "11110", "t6" : "11111",
    
    "a0" : "01010", "a1" : "01011", "a2" : "01100", "a3" : "01101", "a4" : "01110", "a5" : "01111", 
    "a6" : "10000", "a7" : "10001", 
@@ -108,11 +108,13 @@ funct7 = {
 
 # instruction groups
 
-allinst = ["add","sub","slt","sltu","xor",
+allinst = [ 
+           "add","sub","slt","sltu","xor",
            "sll","srl","or","and","addi",
           "sltiu","jalr","sw","beq","bne",
           "blt","bge","bltu","bgeu","lw",
-         "lui","auipc","jal"]
+         "lui","auipc","jal"
+         ]
 
 rtype = ["add","sub","slt","sltu","xor","sll","srl","or","and"]
 
@@ -126,7 +128,7 @@ utype = ["lui","auipc"]
 
 jtype = ["jal"]
 
-# Assembler loop
+# parsing loop
 output = []
 pc = 0 
 for lineno, line in enumerate(program,1):
@@ -134,8 +136,119 @@ for lineno, line in enumerate(program,1):
     x = x.replace("(",",")
     x = x.replace(")", "")
     
-    parts = x.split(",")
+    parts = [p for p in x.split(",") if p != ""]
     op = parts[0]
     
     if op not in allinst:
-    error(lineno)
+        error(lineno)
+
+    # I-type encoding
+    if op in itype:
+        if op == "lw":
+            checkreg(parts[1],lineno)
+            checkreg(parts[3],lineno)
+        
+            checkimm(parts[2],12,lineno)
+        
+            rd = registers[parts[1]]
+            rs1 = registers[parts[3]]
+            imm = binary(parts[2],12)
+            inst = imm + rs1 + funct3[op] + rd + opcode[op]
+        
+            output.append(inst)
+        else :
+            checkreg(parts[1], lineno)
+            checkreg(parts[2], lineno)
+            checkimm(parts[3],12,lineno)
+        
+            rd = registers[parts[1]]
+            rs1 = registers [parts[2]]
+            imm = binary(parts[3],12)
+        
+            inst = imm + rs1 + funct3[op] + rd + opcode[op]
+            output.append(inst)
+    
+        pc+=4
+        continue
+
+    # S-type encoding
+
+    if op in stype:
+        checkreg (parts[1], lineno)
+        checkreg(parts[3], lineno)
+        checkimm(parts[2],12,lineno)
+    
+        rs2 = registers[parts[1]]
+        rs1 = registers[parts[3]]
+        imm = binary(parts[2],12)
+    
+        inst = imm[0:7] + rs2 + rs1 + funct3[op] + imm[7:12] + opcode[op]
+    
+        output.append(inst)
+        pc+=4
+        continue
+
+    # B-type encoding
+    if op in btype:
+        checkreg(parts[1], lineno)
+        checkreg(parts[2], lineno)
+        if parts[3] in labeltable:
+            offset = labeltable[parts[3]] - pc
+        
+        else:
+            offset = int(parts[3])
+    
+        checkimm(offset,13,lineno)
+        imm = binary(offset,13)
+        inst = imm[0] + imm[2:8] + registers[parts[2]] + registers[parts[1]] + funct3[op] + imm[8:12] + imm[1] + opcode[op]
+    
+        output.append(inst)
+        pc +=4
+        continue
+
+    # R-type encoding
+    if op in rtype:
+        checkreg(parts[1], lineno)
+        checkreg(parts[2], lineno)
+        checkreg(parts[3], lineno)
+    
+        rd = registers[parts[1]]
+        rs1 = registers[parts[2]]
+        rs2 = registers[parts[3]]
+    
+        inst = funct7[op] + rs2 + rs1 + funct3[op] + rd + opcode[op]
+        output.append(inst)
+        pc +=4
+        continue
+
+    # J-type encoding
+    if op in jtype:
+        checkreg(parts[1], lineno)
+    
+        if parts[2] in labeltable:
+            offset = labeltable[parts[2]] - pc
+        else:
+            offset = int(parts[2])
+        imm = binary(offset, 21)
+        inst = imm[0] + imm[10:20] + imm[9] + imm[1:9] + registers[parts[1]]  + opcode[op]
+        output.append(inst)  
+        pc +=4
+        continue
+
+    # U-type encoding
+    if op in utype:
+        checkreg (parts[1], lineno)
+        checkimm(parts[2],20,lineno)
+        rd = registers[parts[1]]
+        imm = binary(parts[2],20)
+
+        inst = imm + rd + opcode[op]
+        output.append(inst)
+    
+        pc +=4
+        continue
+
+#output file
+with open (outputfile,"w") as F:
+    for inst in output:
+        F.write(inst + "\n")
